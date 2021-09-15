256d133cf295c04debba28c1e8d5a17191b19741
PhilKagebein <philkagebein@gmail.com>
2021-09-15 10:11:01 -0500

    Initial Commit

--- app/src/main/java/com/example/diceroller_gridlayoutintro/MainActivity.kt
+++ app/src/main/java/com/example/diceroller_gridlayoutintro/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.diceroller_gridlayoutintro
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.diceroller_gridlayoutintro.databinding.ActivityMainBinding
 
@@ -14,12 +13,13 @@ class MainActivity : AppCompatActivity() {
     //Originally dieLIst set using the safe call operator and defined as null. Was getting fucky results though when adding results to dieList down in the setDiceList function. Don't know why
     private var dieList: ArrayList<DieModel> = ArrayList()
     private var whatType = ""
+    lateinit var gridView: GridView
     private lateinit var spinnerDiceType: Spinner
-   // lateinit var dieResults: GridView
     //Needed to initialize the howManyStrArray first before I could use howManySelect function. Why?
     private var howManyStrArray: Array<String> = emptyArray()
     private var diceTypeStrArray: Array<String> = emptyArray()
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +27,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //Everything to make the How Many Spinner
-        howManyStrArray= resources.getStringArray(R.array.saHowManyDice)
+       // ***Originally I used the following line to assign a string aray in the strings.xml to a variable.
+        // then put that variable in the spinner adapter in order to create the spinner. Is there any reason to create the variable? Or is what I have now best practice?****
+        // howManyStrArray= resources.getStringArray(R.array.saHowManyDice)
         val spinnerHowMany = binding.spnHowManyDice
-        val howManyAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, howManyStrArray)
-        spinnerHowMany.adapter = howManyAdapter
+        spinnerHowMany.adapter = ArrayAdapter(this, R.layout.spinner_items, resources.getStringArray(R.array.saHowManyDice))
+
         //HowMany Spinner Item Selected
         spinnerHowMany.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             //tried to return an int here but that didn't work either.
@@ -44,20 +46,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         //Everything to make the Dice Type Spinner
-        diceTypeStrArray = resources.getStringArray(R.array.saDiceType)
+        //diceTypeStrArray = resources.getStringArray(R.array.saDiceType)
         spinnerDiceType = binding.spnDiceType
-        val diceTypeAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, diceTypeStrArray)
-        spinnerDiceType.adapter = diceTypeAdapter
+        spinnerDiceType.adapter = ArrayAdapter(this, R.layout.spinner_items, resources.getStringArray(R.array.saDiceType))
         spinnerDiceType.visibility = View.INVISIBLE
+
         //DiceType Spinner Item Selected
         spinnerDiceType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            //tried to return an int here but that didn't work either.
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 whatType(p2)
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
-          //      binding.tvTest.text = ""
             }
 
         }
@@ -67,8 +67,7 @@ class MainActivity : AppCompatActivity() {
         if(p2 == 0) {
             dieList = ArrayList()
             //Originally had !! after dieList
-            val dieListAdapter = DieResultAdapter(this, dieList)
-            binding.gvDieResults.adapter = dieListAdapter
+            binding.gvDieResults.adapter = DieResultAdapter(this, dieList)
             binding.tvSum.text = ""
             howMany = p2
             spinnerDiceType.visibility = View.INVISIBLE
@@ -77,11 +76,9 @@ class MainActivity : AppCompatActivity() {
             spinnerDiceType.visibility = View.VISIBLE
             //originally had an else here to return p2, didn't work
         }
-
     }
 
     fun whatType(p2: Int) = if(p2==0){
-       // binding.tvTest.text = ""
         whatType = ""
     }else {
         whatType = spinnerDiceType.getItemAtPosition(p2).toString()
@@ -90,49 +87,45 @@ class MainActivity : AppCompatActivity() {
     //why do I need view: View? Android Studio gives me a lil warning guy saying parameter view is never used, but if you take it away, then the app won't load
     fun rollDice(view: View) {
       dieList = ArrayList()
-
-       // binding.tvTest.text = ""
         binding.tvSum.text = ""
-
-        //var diceResult = arrayOf<Int>()
         var sum = 0
         if (howMany == 0 || whatType == "") {
-          //  binding.tvTest.text = ""
             binding.tvSum.visibility = View.GONE
         } else {
             if (howMany == 1) {
-                binding.tvSum.visibility = View.INVISIBLE
+                gridView = findViewById(R.id.gvDieResults)
+                gridView.numColumns = 1
+                binding.tvSum.visibility = View.GONE
                 val whatTypeInt = whatType.substring(1).toInt()
                 //created the var whatTypeInt because when I just changed whatType to an int, the app would crash after rolling twice. I assume it's because of the previous if statement on line 92. Don't know why tho
                 for (i in 1..howMany) {
+                    //Could do another instance of removing the variable diceValue altogether and just throwing ((1..whatTypeInt).random()) into setDiceList. Is better to do?
                     val diceValue = (1..whatTypeInt).random()
                     dieList = setDiceList(diceValue, whatTypeInt)
-                  //  binding.tvTest.append("Die #$i: $diceValue")
-                  //  binding.tvTest.append("\n")
                     sum += diceValue
                 }
             }else{
+                //Originally only had one initilization of gridView on line 104 "gridView = findViewby...." App would crash if I picked any 2-6 dice to start since it wasn't initialized. Is a better way to do this other than writing the same line twice?
+                gridView = findViewById(R.id.gvDieResults)
                 binding.tvSum.visibility = View.VISIBLE
+                when(howMany){
+                    2,3 -> {gridView.numColumns = 1}
+                    4,5,6 -> {gridView.numColumns = 2}
+
+                }
                 val whatTypeInt: Int = whatType.substring(1).toInt()
                 //created the var whatTypeInt because when I just changed whatType to an int, the app would crash after rolling twice. I assume it's because of the previous if statement on line 92. Don't know why tho
                 for (i in 1..howMany) {
                     val diceValue = (1..whatTypeInt).random()
-                   // setDiceList(diceValue)
                     dieList = setDiceList(diceValue, whatTypeInt)
-                  //  binding.tvTest.append("Die #$i: $diceValue")
-                  //  binding.tvTest.append("\n")
                     sum += diceValue
                 }
-                //diceResult.set(i-1, diceValue)
             }
             binding.tvSum.append("Sum: $sum")
 
-            // binding.tvTest.append(diceResult.forEach{ i -> print("$i") }.toString())
         }
-
         //add !! operator to dieList to remove error. why?
-        val dieListAdapter = DieResultAdapter(this, dieList)
-        binding.gvDieResults.adapter = dieListAdapter
+        binding.gvDieResults.adapter = DieResultAdapter(this, dieList)
     }
 
      private fun setDiceList(diceValue: Int, diceTypeInt: Int): ArrayList<DieModel>{
@@ -290,7 +283,6 @@ class MainActivity : AppCompatActivity() {
           12 -> dieList.add(DieModel(d12Images[diceValue-1], d12ImageNames[diceValue-1]))
           20 -> dieList.add(DieModel(d20Images[diceValue-1], d20ImageNames[diceValue-1]))
          }
-
 
          return dieList
     }
