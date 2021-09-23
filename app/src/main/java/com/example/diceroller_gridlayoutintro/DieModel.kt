a7bfcc550b3457610991a422b4185e2eadda646b
PhilKagebein <philkagebein@gmail.com>
2021-09-23 12:22:16 -0500

    -Added a SettingsFragment.kt & root_preferences.xml -Created HomeFragment.kt and migrated most of MainActivity.kt to HomeFragment.kt -Created main_nav.xml -Created topbar_menu.xml -Setup navigation from HomeFragment to SettingsFragment

--- app/src/main/java/com/example/diceroller_gridlayoutintro/DieModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.diceroller_gridlayoutintro
-
-class DieModel(imageIcon: Int?, imageName: String?) {
-
-    var imageIcon: Int? = imageIcon
-    var imageName: String? = imageName
-}
