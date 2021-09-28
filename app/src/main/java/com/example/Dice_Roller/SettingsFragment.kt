6b8bef46276ed54578adb9192f15a0d3c44a33ee
PhilKagebein <philkagebein@gmail.com>
2021-09-28 09:46:04 -0500

    Dark mode switch in settings screen now switches between light and dark mode

--- app/src/main/java/com/example/Dice_Roller/SettingsFragment.kt
+++ app/src/main/java/com/example/Dice_Roller/SettingsFragment.kt
@@ -1,13 +1,37 @@
 package com.example.Dice_Roller
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.example.diceroller_gridlayoutintro.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-      }
+
+        var darkMode = findPreference<Preference>("dark_mode")
+        darkMode?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener {
+                darkMode, _ -> changeThemeMode()
+        }
+
+        }
+
+    private fun changeThemeMode(): Boolean{
+        val sp = PreferenceManager.getDefaultSharedPreferences(context)
+        val darkModeValue = sp.getBoolean("dark_mode", true)
+
+        if(!darkModeValue) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+        return true
+    }
+
+
 
 }
+
